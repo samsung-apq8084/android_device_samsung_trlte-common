6cf571fac45e59b4d7cb2d6466577c78fcd989df
underscoremone <underscoremone@gmail.com>
2017-10-27 00:20:45 +0100

    trlte-common: trltexx mixer_paths.xml workaround 1/2
    trltexx and trltedt require a different mixer_paths.xml.
    Add a small script to copy the right file during installation.
    
    Change-Id: I3fe7e2d9c08b523052c89b8af9d5e38055341ae3

--- audio/Android.mk
+++ audio/Android.mk
@@ -1,15 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-ifeq ($(TARGET_DEVICE),trltexx)
-include $(CLEAR_VARS)
-LOCAL_MODULE       := mixer_paths.xml
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := mixer_paths_eur.xml
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
-include $(BUILD_PREBUILT)
-else
 include $(CLEAR_VARS)
 LOCAL_MODULE       := mixer_paths.xml
 LOCAL_MODULE_TAGS  := optional
@@ -17,4 +8,4 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := mixer_paths.xml
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
-endif
+
