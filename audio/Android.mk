a773818ff4d12de1f648c1f03e4e0700f08564a5
micky387 <mickaelsaibi@free.fr>
2018-02-03 03:37:04 +0100

    Add support of tblte-common (Note Edge)

--- audio/Android.mk
+++ audio/Android.mk
@@ -9,12 +9,34 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := mixer_paths_eur.xml
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
-else
+endif
+
+ifeq ($(TARGET_DEVICE),trlte trltetmo trltespr trlteduos trltevzw)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := mixer_paths.xml
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := mixer_paths.xml
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+include $(BUILD_PREBUILT)
+endif
+
+ifeq ($(TARGET_DEVICE),tbltexx)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := mixer_paths.xml
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := mixer_paths_tblte_eur.xml
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+include $(BUILD_PREBUILT)
+endif
+
+ifeq ($(TARGET_DEVICE),tblte tbltetmo tbltespr tblteduos tbltevzw)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := mixer_paths.xml
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := mixer_paths_tblte.xml
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 endif
